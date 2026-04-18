bdc0e3895be907f2302e3d23d4e0a44f2cfc7fb6
Mitch <mitchhome@The-Machine.local>
2026-04-18 12:03:36 -0400

    Temp Sensor Logic Updates

--- temp_sensor.py
+++ temp_sensor.py
@@ -3,9 +3,17 @@ from grove.grove_temperature_sensor import GroveTemperatureSensor
 class TSensor:
     def __init__(self, channel):
         self.sensor = GroveTemperatureSensor(channel)
+        self.temp_reads = []
+
+    def get_last_read_temp(self):
+        if (len(self.temp_reads) == 0):
+            return None
+        
+        return self.temp_reads[len(self.temp_reads) - 1]
 
     def read_temp(self):
-        return self.sensor.temperature
+        self.temp_reads.append(self.sensor.temperature)
+        return self.get_last_read_temp()
 
-    def format_temp(self, temperature):
-        return 'Temperature: {0:.2f} °C'.format(temperature)
+    def formatted_temp(self):
+        return 'Temperature: {0:.2f} °C'.format(self.get_last_read_temp())
