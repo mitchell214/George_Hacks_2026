2a6159a38a6c54d8914d93accfaaa637224e8be0
mitchell214 <mitchellc.sdavis@gmail.com>
2026-04-18 17:22:29 -0400

    Delete uv_sensor.py

--- uv_sensor.py
+++
@@ -1,8 +0,0 @@
-from grove.grove_i2c_uv_sensor_veml6070 import GroveI2CUVLightSensor
-
-class UVSensor:
-    def __init__(self):
-        self.sensor = GroveI2CUVLightSensor()
-
-    def read_uv_intensity(self):
-        return self.sensor.get_uv_intensity()
